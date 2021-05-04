8a1d7b8ca06710a89b9d483f7001c8d08d0e51cb
Dayo Ojo Jnr <dayo@kudabank.com>
2021-05-04 22:41:25 +0100

    feat: added database settings, added model, added service

---
+++ Config/DatabaseSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecurePrivacyExercise.Config
+{
+    public class DatabaseSettings : IDatabaseSettings
+    {
+        public string StudentCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IDatabaseSettings
+    {
+        string StudentCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
